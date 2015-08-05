9d2ace6692ee4254467f448dce86072fd1a2410f
Chandradasa <davechandradasa@Daves-Air.home>
2015-08-05 13:05:21 +0100

    re Submission  Stage 4
    taken out cgi module, added content on why validation is important.
    Tried to intent python  code , to 4 spaces, but errors occurred . Had
    to revert to  2 spaces

--- main.py
+++ main.py
@@ -18,7 +18,6 @@
 import os
 import webapp2
 import jinja2
-import cgi
 import urllib
 
 from google.appengine.api import users
@@ -42,11 +41,11 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template,**kw))
 
 class CourseNotes(Handler):
-      def get (self):       
+    def get (self):       
          self.render('index.html')  
 
 class Comments(Handler):
-      def get (self):     
+    def get (self):     
          self.render('comments.html')  
 
 
@@ -82,9 +81,9 @@ class Post(ndb.Model):
 
 
 class MainPage(Handler):
-  def get(self):
-    wall_name = self.request.get('wall_name',DEFAULT_WALL)
-    if wall_name == DEFAULT_WALL.lower(): wall_name = DEFAULT_WALL
+    def get(self):
+        wall_name = self.request.get('wall_name',DEFAULT_WALL)
+        if wall_name == DEFAULT_WALL.lower(): wall_name = DEFAULT_WALL
 
     # Ancestor Queries, as shown here, are strongly consistent
     # with the High Replication Datastore. Queries that span
@@ -94,74 +93,74 @@ class MainPage(Handler):
     # query.
 
     # [START query]
-    posts_query = Post.query(ancestor = wall_key(wall_name)).order(-Post.date)
+        posts_query = Post.query(ancestor = wall_key(wall_name)).order(-Post.date)
 
     # The function fetch() returns all posts that satisfy our query. The function returns a list of
     # post objects
-    posts =  posts_query.fetch()
+        posts =  posts_query.fetch()
     # [END query]
 
     # If a person is logged into Google's Services
-    user = users.get_current_user()
-    if user:
-        url = users.create_logout_url(self.request.uri)
-        url_linktext = 'Logout'
-        user_name = user.nickname()
-    else:
-        url = users.create_login_url(self.request.uri)
-        url_linktext = 'Login'
-        user_name = 'Anonymous Poster'
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            user_name = user.nickname()
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+            user_name = 'Anonymous Poster'
 
     
-    error = self.request.get('error')
+        error = self.request.get('error')
 
-    sign_query_params = urllib.urlencode({'wall_name': wall_name})
+        sign_query_params = urllib.urlencode({'wall_name': wall_name})
 
-    posts_html=" " 
-    rendered_HTML =  {"sign_query_params" : sign_query_params, "wall_name" : wall_name, "user_name" :user_name,
+        posts_html=" " 
+        rendered_HTML =  {"sign_query_params" : sign_query_params, "wall_name" : wall_name, "user_name" :user_name,
                                    "url":url, "url_linktext": url_linktext, "posts": posts,"error":error }
 
     # Write Out Page here
     #self.response.out.write(rendered_HTML)
-    self.render("comments.html",**rendered_HTML)  
+        self.render("comments.html",**rendered_HTML)  
  
 
 class PostWall(Handler):
-  def post(self):
+    def post(self):
     # We set the same parent key on the 'Post' to ensure each
 
     # Post is in the same entity group. Queries across the
     # single entity group will be consistent. However, the write
     # rate to a single entity group should be limited to
     # ~1/second.
-    wall_name = self.request.get('wall_name',DEFAULT_WALL)
-    post = Post(parent=wall_key(wall_name))
+        wall_name = self.request.get('wall_name',DEFAULT_WALL)
+        post = Post(parent=wall_key(wall_name))
 
     # When the person is making the post, check to see whether the person
     # is logged into Google
-    if users.get_current_user():
-      post.author = Author(
-            identity=users.get_current_user().user_id(),
-            name=users.get_current_user().nickname(),
-            email=users.get_current_user().email())
-    else:
-      post.author = Author(
-            name='anonymous@anonymous.com',
-            email='anonymous@anonymous.com')
+        if users.get_current_user():
+            post.author = Author(
+                identity=users.get_current_user().user_id(),
+                name=users.get_current_user().nickname(),
+                email=users.get_current_user().email())
+        else:
+            post.author = Author(
+                name='anonymous@anonymous.com',
+                email='anonymous@anonymous.com')
 
 
     # Get the content from our request parameters, in this case, the message
     # is in the parameter 'content'
     
   
-    post.content = self.request.get('content')
-    if post.content:
+        post.content = self.request.get('content')
+        if post.content:
 
     # Write to the Google Database
-        post.put()
-        self.redirect('/MainPage')
-    else:  
-        self.redirect('/MainPage?error= Please add comments before submitting')
+            post.put()
+            self.redirect('/MainPage')
+        else:  
+            self.redirect('/MainPage?error= Please add comments before submitting')
 
     # Do other things here such as a page redirect
 
